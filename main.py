b0cd5d9e8e4d1327455d0c0f1c4876c1d9637c3d
Dylan Buchi <devybuchi@gmail.com>
2020-10-24 14:01:28 -0300

    add data form to data folder csv

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-from flask import Flask, render_template
+import csv
+import os
+
+from flask import Flask, request, redirect
+from flask.templating import render_template
 app = Flask(__name__)
+
+
+def write_to_csv_file(data: dict):
+    path = os.path.join(os.getcwd(), 'data', 'emails.csv')
+    with open(path, 'a', newline='') as csv_file:
+        email = data['name']
+        subject = data['email']
+        message = data['message']
+
+        csv_writer = csv.writer(csv_file,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/send_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv_file(data)
+
+        return redirect('thanks.html')
+    else:
+        return 'error'
 
 
 @app.route('/')
