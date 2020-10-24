ee9226a16632cae7c8e326e1861101458a1f4dea
Dylan Buchi <devybuchi@gmail.com>
2020-10-23 23:20:33 -0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def home_route():
+    return render_template('index.html')
+
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def pages(page_name):
+    return render_template(page_name)
+
+
+if __name__ == '__main__':
+    app.run()
